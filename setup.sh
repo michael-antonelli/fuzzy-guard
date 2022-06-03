ac29b518e494455f0053952cb32fb46434762a95
Michael Antonelli <mantone2@tphysdptcit1.services.brown.edu>
2022-06-03 08:48:07 -0400

    changed fg to fuzz

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# ensure docker is installed
 # source environmental vars from config file
-# add /opt/fuzz/bin to $PATH
 # Create vuls user
 # Generate ssh keys for VULS_USER
+# add /opt/fuzz/bin to $PATH for vuls user
 # cron setup
