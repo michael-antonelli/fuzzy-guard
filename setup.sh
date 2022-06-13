e2691578c1c3a056278c742c6aced57df922f17a
Michael Antonelli <mantone2@tphysdptcit1.services.brown.edu>
2022-06-13 09:52:51 -0400

    subcommand progress

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# ensure docker is installed
+# ensure docker, python is installed
 # source environmental vars from config file
 # Create vuls user
+# add vuls user to docker group
 # Generate ssh keys for VULS_USER
 # add /opt/fuzz/bin to $PATH for vuls user
+# add env vars to vuls bashrc
 # cron setup
